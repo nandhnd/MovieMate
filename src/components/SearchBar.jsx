a2631b6f757e32e61c736bc62637633543a99fc3
nandhnd <nandahandika004@gmail.com>
2026-05-12 00:20:36 +0700

    add Input Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,81 @@
-import { StyleSheet, TextInput, View } from "react-native";
-import { Search } from "lucide-react-native";
+import React, { useRef, useEffect } from "react";
+import { StyleSheet, View, TextInput, Animated, Pressable } from "react-native";
+import { Search, ArrowLeft, X } from "lucide-react-native";
 import { colors } from "../../assets/theme";
+import { useNavigation } from "@react-navigation/native";
 
-const SearchBar = ({ placeholder, value, onChangeText }) => {
+const SearchBar = ({ searchPhrase, setSearchPhrase }) => {
+  const navigation = useNavigation();
+  const animation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Search size={18} color={colors.grey(0.5)} />
-      <TextInput
-        style={styles.input}
-        placeholder={placeholder || "Search movies..."}
-        placeholderTextColor={colors.grey(0.5)}
-        value={value}
-        onChangeText={onChangeText}
-      />
-    </View>
+    <Animated.View
+      style={[
+        styles.container,
+        {
+          gap: animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 12],
+          }),
+        },
+      ]}
+    >
+      <Animated.View
+        style={{
+          transform: [
+            {
+              scale: animation.interpolate({
+                inputRange: [0, 0.8, 1],
+                outputRange: [0, 1.2, 1],
+              }),
+            },
+          ],
+        }}
+      >
+        <Pressable
+          onPress={() => navigation.goBack()}
+          style={({ pressed }) => ({ opacity: pressed ? 0.6 : 1 })}
+        >
+          <ArrowLeft color={colors.grey(0.6)} size={24} />
+        </Pressable>
+      </Animated.View>
+
+      <View style={styles.bar}>
+        <Search
+          size={18}
+          color={searchPhrase ? colors.black() : colors.grey(0.5)}
+        />
+        <TextInput
+          style={styles.textinput}
+          placeholder="Search movies..."
+          placeholderTextColor={colors.grey(0.5)}
+          value={searchPhrase}
+          onChangeText={setSearchPhrase}
+          borderWidth={0}
+          underlineColorAndroid="transparent"
+          autoCorrect={false}
+          autoFocus={true}
+          returnKeyType="search"
+          clearButtonMode="while-editing"
+        />
+        {searchPhrase && (
+          <Pressable
+            onPress={() => setSearchPhrase("")}
+            style={({ pressed }) => ({ opacity: pressed ? 0.6 : 1 })}
+          >
+            <X size={18} color={colors.black()} />
+          </Pressable>
+        )}
+      </View>
+    </Animated.View>
   );
 };
 
@@ -22,20 +84,24 @@ export default SearchBar;
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  bar: {
+    flexDirection: "row",
+    padding: 12,
+    gap: 10,
     alignItems: "center",
     backgroundColor: colors.grey(0.05),
     borderRadius: 12,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    gap: 10,
-    marginHorizontal: 24,
-    marginVertical: 12,
-  },
-  input: {
     flex: 1,
-    fontFamily: "Pjs-Regular",
+  },
+  textinput: {
     fontSize: 14,
+    fontFamily: "Pjs-Medium",
     color: colors.black(),
+    lineHeight: 18,
     padding: 0,
+    flex: 1,
   },
 });
